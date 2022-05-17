3cff88d4577f81669698696d7836d76e830e570d
maikalangi <maikalangi@gmail.com>
2022-05-16 21:33:17 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get('/greeting/:name', (req, res)=>{
         res.send(`Hello, ${req.params.name}`);
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res)=>{
+    res.send(`${(req.params.tipPercentage/100)*req.params.total}`);
+});
+
 app.listen(port, ()=>{
     console.log(`Express is listening for requests from port ${port}`);
 });
